0654c4c55a99be7d8b6e02f5edfddada97a0f5ca
alwahid786 <wahidforjob@gmail.com>
2025-03-19 14:55:38 +0500

    feat: sign, login, about and terms of services pages ui done

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <section className="w-full h-svh md:h-screen overflow-y-scroll scroll-0 bg-[#f0f0f0] md:bg-[url('src/assets/images/authimage.png')] bg-contain bg-no-repeat bg-left">
       <section className="container mx-auto px-5 py-8 md:py-14 w-full h-full grid grid-cols-1 lg:grid-cols-12">
-        <div className="lg:col-span-6"></div>
+        <div className="lg:col-span-6 hidden lg:block"></div>
         <div className="lg:col-span-6 grid place-items-center">
           <div className="w-full rounded-lg py-5 px-4">
             <h6 className="text-center text-gray-950 text-xl md:text-2xl font-semibold">
