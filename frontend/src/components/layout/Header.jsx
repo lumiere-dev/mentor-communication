2eb80ac6c5df6d5a7b8b1c2b0283156d5aa2aea6
alwahid786 <wahidforjob@gmail.com>
2025-03-19 14:52:41 +0500

    feat: sign, login, about and terms of services pages ui done

---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,41 @@
+import { Link, useLocation } from "react-router-dom";
+
+const pages = [
+  {
+    title: "About Us",
+    link: "/about",
+  },
+  {
+    title: "Terms of Service",
+    link: "/terms-of-service",
+  },
+];
+
+const Header = () => {
+  const { pathname } = useLocation();
+  console.log("pathname", pathname);
+  return (
+    <header className="w-full bg-blue-600">
+      <section className="container mx-auto flex items-center justify-between h-[70px]">
+        <Link to="/" className="text-white font-bold text-sm md:text-base">
+          Mentor <span className="font-normal">Communication</span>
+        </Link>
+        <div className="flex items-center gap-4">
+          {pages.map((page, i) => (
+            <Link
+              to={page.link}
+              key={i}
+              className={`text-sm md:text-base font-medium ${
+                pathname === page.link ? "text-orange-300" : "text-white"
+              }`}
+            >
+              {page.title}
+            </Link>
+          ))}
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
