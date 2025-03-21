cc950d4a013d0a6adc663554934a403c24d8a9c5
alwahid786 <wahidforjob@gmail.com>
2025-03-21 17:21:00 +0500

    fix image

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   };
   return (
     <section
-      className={`w-full h-svh md:h-screen overflow-y-scroll scroll-0 bg-[#f0f0f0] md:bg-[url('src/assets/images/authimage.png')]  bg-contain bg-no-repeat bg-left`}
+      className={`w-full h-svh md:h-screen overflow-y-scroll scroll-0 bg-[#f0f0f0] md:bg-[url('https://res.cloudinary.com/hamzanafasat/image/upload/v1742559578/hymhem1uuburthm3aewg.png')]  bg-contain bg-no-repeat bg-left`}
     >
       <section className="container mx-auto px-5 py-8 md:py-14 w-full h-full grid grid-cols-1 lg:grid-cols-12">
         <div className="lg:col-span-6 hidden lg:block"></div>
