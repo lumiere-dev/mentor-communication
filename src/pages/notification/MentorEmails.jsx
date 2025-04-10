6b0ead8b5a6c9da1fbb5e04bd8c55a0dddbb28a9
alwahid786 <wahidforjob@gmail.com>
2025-04-10 17:04:31 +0500

    push latest code

--- src/pages/notification/MentorEmails.jsx
+++ src/pages/notification/MentorEmails.jsx
@@ -11,28 +11,28 @@ const MentorEmails = () => {
       name: "Student Email",
       selector: (row) => row?.studentEmail,
       sortable: true,
-      width: "15%",
+      width: "17%",
       cell: (row) => <div className="truncate font-medium">{row?.studentEmail}</div>,
     },
     {
       name: "Mentor Email",
       selector: (row) => row?.mentorEmail,
       sortable: true,
-      width: "15%",
+      width: "17%",
       cell: (row) => <div className="truncate font-medium">{row?.mentorEmail}</div>,
     },
     {
       name: "Parent Email",
       selector: (row) => row?.parentEmail,
       sortable: true,
-      width: "15%",
+      width: "17%",
       cell: (row) => <div className="truncate font-medium">{row?.parentEmail}</div>,
     },
     {
       name: "Last Sender",
       selector: (row) => row?.latest_sender,
       sortable: true,
-      width: "10%",
+      width: "17%",
       cell: (row) => (
         <div className="flex items-center">
           <div className="flex items-center">
@@ -67,15 +67,15 @@ const MentorEmails = () => {
       name: "Flag Count",
       selector: (row) => row?.totalFlagCount,
       sortable: true,
-      width: "25%",
+      width: "10%",
       cell: (row) => <div className="truncate font-medium">{row?.totalFlagCount}</div>,
     },
     {
       name: "Last Message Time",
-      selector: (row) => row?.latestTimestamp,
+      selector: (row) => row?.lastMessageCreatedAt,
       sortable: true,
       width: "20%",
-      cell: (row) => <div className="truncate font-medium">{row?.latestTimestamp}</div>,
+      cell: (row) => <div className="truncate font-medium">{row?.lastMessageCreatedAt}</div>,
     },
   ];
 
